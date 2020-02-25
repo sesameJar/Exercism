978888131192e21ce1fe70da397f0c6c088c0d04
Mehrad Kavian <mehrad.kavian@gmail.com>
2020-02-25 15:17:57 -0500

    handeling warn from clippy

--- beer-song/src/lib.rs
+++ beer-song/src/lib.rs
@@ -3,16 +3,16 @@ use std::fmt::Write;
 pub fn verse(n: u32) -> String {
     let mut s = String::new();
     if n == 0 {
-        write!(&mut s, "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n");
+        write!(&mut s, "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n").unwrap();
     }
     if n == 1 {
-        write!(&mut s, "{} bottle of beer on the wall, {} bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n", n,n);
+        writeln!(&mut s, "{} bottle of beer on the wall, {} bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n", n,n).unwrap();
     }
     if n > 1 && n < 100 {
         if n == 2 {
-            write!(&mut s, "{} bottles of beer on the wall, {} bottles of beer.\nTake one down and pass it around, {} bottle of beer on the wall.\n", n,n, n-1);
+            writeln!(&mut s, "{} bottles of beer on the wall, {} bottles of beer.\nTake one down and pass it around, {} bottle of beer on the wall.\n", n,n, n-1).unwrap();
         } else {
-            write!(&mut s, "{} bottles of beer on the wall, {} bottles of beer.\nTake one down and pass it around, {} bottles of beer on the wall.\n", n,n, n-1);
+            writeln!(&mut s, "{} bottles of beer on the wall, {} bottles of beer.\nTake one down and pass it around, {} bottles of beer on the wall.\n", n,n, n-1).unwrap();
         }
     }
 
@@ -22,7 +22,7 @@ pub fn verse(n: u32) -> String {
 pub fn sing(start: u32, end: u32) -> String {
     let mut s = String::new();
     for i in (end..=start).rev() {
-        write!(&mut s,"{}\n", verse(i));
+        writeln!(&mut s, "{}\n", verse(i)).unwrap();
     }
-    s[0..s.len()-1].to_string()
+    s[0..s.len() - 1].to_string()
 }
