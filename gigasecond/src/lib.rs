0495ab1c5c8bd715b62418e6ba4eb2e30350427c
Mehrad Kavian <mehrad.kavian@gmail.com>
2020-03-02 13:39:33 -0500

    cleaning gigaseconds

--- gigasecond/src/lib.rs
+++ gigasecond/src/lib.rs
@@ -1,10 +1,8 @@
-use chrono::{DateTime, FixedOffset, TimeZone, Utc};
+use chrono::{DateTime, TimeZone, Utc};
 
 // Returns a Utc DateTime one billion seconds after start.
 pub fn after(start: DateTime<Utc>) -> DateTime<Utc> {
-    let mut ts = start.timestamp_millis() + 1e12 as i64;
-    // ts += 10i64.pow(9);
-    // Utc.timestamp(ts, 0)
-    
+    let ts = start.timestamp_millis() + 1e12 as i64;
+
     Utc.timestamp_millis(ts)
 }
