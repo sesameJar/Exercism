7d47e619744f81d9afaf7f4ff55c974a3213e3a1
Mehrad Kavian <mehrad.kavian@gmail.com>
2020-03-09 13:13:37 -0400

    using duration for Gigasecond exercise

--- gigasecond/src/lib.rs
+++ gigasecond/src/lib.rs
@@ -1,8 +1,8 @@
-use chrono::{DateTime, TimeZone, Utc};
+use chrono::{DateTime, Duration, TimeZone, Utc};
+use std::ops::Add;
 
 // Returns a Utc DateTime one billion seconds after start.
 pub fn after(start: DateTime<Utc>) -> DateTime<Utc> {
-    let ts = start.timestamp_millis() + 1e12 as i64;
-
-    Utc.timestamp_millis(ts)
+    let sec_dur = Duration::seconds(1e9 as i64);
+    start.add(sec_dur)
 }
